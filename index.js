117ad2c8d65b1ba67ccbcc947af53913b8acfb03
Md. Sindabad <pavel.sindabad@gmail.com>
2021-10-29 12:29:49 +0600

    db connected

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient } = require("mongodb");
+require('dotenv').config();
 
 
 // initialize express app
@@ -9,10 +11,28 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// routes
-app.get('/', (req, res) => {
-    res.send("Welcome to paradise travel...")
-})
+// database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.PASS}@cluster0.bmdl2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('paradise_travel');
+        const destinations = database.collection('destinations');
+
+        app.get('/', async (req, res) => {
+            const cursor = destinations.find({});
+            const data = await cursor.toArray();
+            res.json(data)
+        })
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 const port = process.env.PORT || 5000;
 // start server
